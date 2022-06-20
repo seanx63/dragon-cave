4975cffe410cb9761dffdbe35d5a40eeb4e764a6
seanx63 <seanx63@gmail.com>
2022-06-20 11:59:15 -0700

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # dragon-cave
-GenSpark Project 1
+## A simple test game written in Java. Dragons live in caves full of their hoarded treasure, some are willing to share their treasure, some aren't so friendly.
---
+++ dragonCave.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class dragonCave {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in); // Initialize Scanner object to read player input
+		System.out.print(""" 
+			You are in a land full of dragons. In front of you,
+			you see two caves. In one cave, the dragon is friendly
+			and will share his treasure with you. The other dragon
+			is greedy and hungry and will eat you on sight.
+			Which cave will you go into? ('one' or 'two')
+			> """); // Intro text
+		String playerChoice = in.nextLine(); // Read next string to allow for different user input
+		in.close(); // Close Scanner for safety
+		switch (playerChoice.toLowerCase()) { // Use the player input variable with the toLowerCase method for case insensitivity
+			case "1", "one" ->
+				System.out.println("""
+						You cautiously approach the cave...
+						The dragon eyes you carefully.
+						One of it's imposing claws steadily approaches you...
+						...And swipes at you with enough force to fell a giant!
+						YOU LOSE! 
+						""");
+			case "2", "two" ->
+				System.out.println("""
+						You cautiously approach the cave...
+						The dragon eyes you carefully.
+						One of it's imposing claws steadily approaches you...
+						...And in it's paw is enough gold to become ROYALTY!
+						YOU WIN!
+						""");
+			default ->
+				System.out.println("""
+					Not choosing between cave 'one' or 'two', 
+					you decide to head home and accidentally
+					trip on a stool.
+					YOU LOSE!""");
+		}
+    }
+}
